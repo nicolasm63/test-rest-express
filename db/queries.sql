a313179439d1581c26150ed8e7f31e3f15fbc6fa
Nicolas Miret <nicolas.miret@yousign.com>
2022-08-10 10:29:51 +0200

    Add query file for statistics

---
+++ db/queries.sql
@@ -0,0 +1,13 @@
+SELECT
+	groupSize,
+	CAST(COUNT(id) AS FLOAT)/(SELECT COUNT(id) FROM student)*100 AS studentsPercentage
+FROM (
+  SELECT student.id, COUNT(student_group.group_id) AS groupSize 
+  FROM student
+  LEFT JOIN student_group ON student.id = student_group.student_id
+  GROUP BY student.id
+) AS t1
+GROUP BY groupSize
+ORDER BY groupSize ASC;
+
+SELECT * FROM "group" WHERE creation_date > (NOW() - INTERVAL '1 year');
